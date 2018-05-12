79e1aaa73bb048693bfd5021699086b9ad5e370c
HOJA.M.A <hoja.ma@pitsolutions.com>
2018-05-12 21:04:52 +0000

    autoload added

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '3.0.0',
+	'version' => '3.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,6 +39,12 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
+	),
+	'autoload' => 
+	array(
+		'classmap' => array(
+			'pi/'
+		)
 	),
 	'_md5_values_when_last_written' => 'a:25:{s:12:"ext_icon.gif";s:4:"837c";s:17:"ext_localconf.php";s:4:"339c";s:14:"ext_tables.php";s:4:"7eb7";s:14:"ext_tables.sql";s:4:"9dd9";s:28:"ext_typoscript_constants.txt";s:4:"4a6a";s:28:"ext_typoscript_editorcfg.txt";s:4:"40c6";s:24:"ext_typoscript_setup.txt";s:4:"0ed1";s:31:"icon_tx_abminijoboffers_cat.gif";s:4:"5a18";s:34:"icon_tx_abminijoboffers_cat__h.gif";s:4:"ebfb";s:35:"icon_tx_abminijoboffers_contact.gif";s:4:"fe28";s:38:"icon_tx_abminijoboffers_contact__h.gif";s:4:"6811";s:32:"icon_tx_abminijoboffers_jobs.gif";s:4:"7eb0";s:35:"icon_tx_abminijoboffers_jobs__h.gif";s:4:"e148";s:36:"icon_tx_abminijoboffers_jobs__ht.gif";s:4:"56ab";s:35:"icon_tx_abminijoboffers_jobs__t.gif";s:4:"9c97";s:36:"icon_tx_abminijoboffers_jobs__tu.gif";s:4:"c309";s:35:"icon_tx_abminijoboffers_jobs__u.gif";s:4:"54cf";s:16:"locallang_db.php";s:4:"a338";s:7:"tca.php";s:4:"51bc";s:14:"doc/manual.sxw";s:4:"12db";s:19:"doc/wizard_form.dat";s:4:"0b59";s:20:"doc/wizard_form.html";s:4:"8b89";s:36:"pi1/class.tx_abminijoboffers_pi1.php";s:4:"d65c";s:17:"pi1/locallang.php";s:4:"ef83";s:31:"pi1/minijoboffers_template.tmpl";s:4:"2181";}',
 	'suggests' => array(
